4fc7996ae87c6b24933b07534dcfa49a9a27e994
Jevaughn Smith <smithjevaughn20@gmail.com>
2021-07-26 02:57:32 -0500

    The calculator can now perform calculations

--- script.js
+++ script.js
@@ -54,6 +54,23 @@ function updateScreen(buttonValue){
             screenText.innerText = screenTextValues;
             toggleBlinkingEffect();
             break;
+        
+        case "=":
+            let newExpression = screenTextValues.replace(/×/g, "*");
+            newExpression = newExpression.replace(/÷/g, "/");
+            newExpression = newExpression.replace(/−/g, "-");
+
+            let newExpressionLastChar = newExpression[newExpression.length - 1];
+            
+            if(newExpressionLastChar == "%"){
+                newExpression = newExpression.replace(/%/g, "*0.01*");
+                newExpression = newExpression.replace(/\*\*/g, "*");
+                newExpression = newExpression.slice(0, newExpression.length - 1);
+            }
+
+            screenTextValues = `${eval(newExpression)}`;
+            screenText.innerText = screenTextValues;
+            break;
 
         default:
             //Ensures that the vertical bar does not stay in front of the text
