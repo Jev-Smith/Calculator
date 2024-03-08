3e845119c85c6c7ff7cf1f0cf0218a91fd973269
Jevaughn Smith <smithjevaughn20@gmail.com>
2024-03-08 11:31:14 -0500

    Hid insertion point when entering values

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const log = console.log;
 const screenText = document.querySelector(".screen-text");
 const buttons = document.querySelectorAll(".button");
 
@@ -9,28 +9,88 @@ let tempString = "";
 let charArray = ["÷", "×", "−", "+"];
 let charArray2 = ["÷", "×", "+"];
 
+function toggleBlinkingEffect() {
+    if (screenText.innerText == "|") {
+        screenText.classList.add("insertion-point");
+    } else {
+        screenText.classList.remove("insertion-point");
+    }
+}
+
+screenText.addEventListener('keyup', (e) => {
+    let keyboardInput = e.target.innerText;
+    // console.log(keyboardInput);
+    defaultBehaviour(keyboardInput);
+})
+
+screenText.addEventListener('keydown', (e) => {
+    if (e.target.innerText === '|') {
+        e.target.innerText = '';
+    }
+})
+
 //Adds an eventListener to each button
 buttons.forEach(button => {
     addListener(button);
 });
 
-function addListener(button){
+function addListener(button) {
     button.addEventListener("click", () => getInnerText(button));
 }
 
-function getInnerText(button){
+function getInnerText(button) {
     let buttonValue = button.firstElementChild.innerText;
     updateScreen(buttonValue);
 }
 
-function errorMessage(){
+function errorMessage() {
     screenTextValues = "Error";
     screenText.innerText = screenTextValues;
 }
 
-function updateScreen(buttonValue){
+function defaultBehaviour(buttonValue) {
+    //Ensures that the vertical bar does not stay in front of the text
+    if (screenTextValues == "|") {
+        screenTextValues = "";
+    }
 
-    switch (buttonValue){
+    if (screenTextValues == "Error" || screenTextValues == "Infinity") {
+        screenTextValues = "";
+    }
+
+    //Start 
+    //This section of code prevents certain operators from being typed in succession
+    tempString += buttonValue;
+
+    let lastCharIndex = tempString.length - 1;
+    let tempStringLastChar = tempString[lastCharIndex];
+    let tempStringLastCharIndex = tempString.lastIndexOf(tempStringLastChar);
+
+    let secondToLastCharIndex = tempString.length - 2;
+    let secondToLastChar = tempString[secondToLastCharIndex];
+
+    let screenTextValuesLastIndex = screenTextValues.length - 1;
+    let screenTextValuesLastChar = screenTextValues[screenTextValuesLastIndex];
+
+    if (charArray.includes(tempStringLastChar) && tempStringLastCharIndex == lastCharIndex &&
+        charArray.includes(secondToLastChar)) {
+
+        buttonValue = "";
+    }
+
+    if (charArray2.includes(screenTextValuesLastChar) && tempStringLastChar == "−") {
+        buttonValue = tempStringLastChar;
+    }
+    //End
+
+    screenTextValues += buttonValue;
+    screenText.innerText = screenTextValues;
+    toggleBlinkingEffect();
+}
+
+function updateScreen(buttonValue) {
+
+    switch (buttonValue) {
 
         case "AC":
             screenTextValues = "";
@@ -42,7 +102,7 @@ function updateScreen(buttonValue){
         case "C":
             let secondToLastCharacter = screenTextValues[screenTextValues.length - 2];
 
-            if(screenTextValues == "Error" || screenTextValues == "Infinity"){
+            if (screenTextValues == "Error" || screenTextValues == "Infinity") {
                 tempString = "";
                 screenTextValues = "|";
                 screenText.innerText = screenTextValues;
@@ -50,16 +110,16 @@ function updateScreen(buttonValue){
                 break;
             }
 
-            if(charArray.includes(secondToLastCharacter)){
+            if (charArray.includes(secondToLastCharacter)) {
                 tempString += secondToLastCharacter;
             }
-            else{
+            else {
                 tempString = "";
             }
 
             slicedString = screenTextValues.slice(0, -1);
 
-            if (slicedString == ""){
+            if (slicedString == "") {
                 slicedString = "|";
             }
 
@@ -67,22 +127,22 @@ function updateScreen(buttonValue){
             screenText.innerText = screenTextValues;
             toggleBlinkingEffect();
             break;
-        
+
         case "=":
-            try{
+            try {
                 let regex = /(\*%|\/%|\+%|-%)/g;
                 let newExpression = screenTextValues.replace(/×/g, "*");
                 newExpression = newExpression.replace(/÷/g, "/");
                 newExpression = newExpression.replace(/−/g, "-");
 
-                if(regex.test(newExpression)){
+                if (regex.test(newExpression)) {
                     errorMessage();
                     break;
                 }
 
                 let newExpressionLastChar = newExpression[newExpression.length - 1];
-                
-                if(newExpressionLastChar == "%"){
+
+                if (newExpressionLastChar == "%") {
                     newExpression = newExpression.replace(/%/g, "*0.01*");
                     newExpression = newExpression.replace(/\*\*/g, "*");
                     newExpression = newExpression.slice(0, newExpression.length - 1);
@@ -91,56 +151,12 @@ function updateScreen(buttonValue){
                 screenTextValues = `${eval(newExpression)}`;
                 screenText.innerText = screenTextValues;
             }
-            catch{
+            catch {
                 errorMessage();
             }
             break;
 
         default:
-            //Ensures that the vertical bar does not stay in front of the text
-            if (screenTextValues == "|"){
-                screenTextValues = "";
-            }
-            
-            if (screenTextValues == "Error" || screenTextValues == "Infinity"){
-                screenTextValues = "";
-            }
-
-            //Start 
-            //This section of code prevents certain operators from being typed in succession
-            tempString += buttonValue;
-        
-            let lastCharIndex = tempString.length - 1;
-            let tempStringLastChar = tempString[lastCharIndex];
-            let tempStringLastCharIndex = tempString.lastIndexOf(tempStringLastChar);
-
-            let secondToLastCharIndex = tempString.length - 2;
-            let secondToLastChar = tempString[secondToLastCharIndex];
-
-            let screenTextValuesLastIndex = screenTextValues.length - 1;
-            let screenTextValuesLastChar = screenTextValues[screenTextValuesLastIndex];
-
-            if (charArray.includes(tempStringLastChar) && tempStringLastCharIndex == lastCharIndex && 
-                charArray.includes(secondToLastChar)){
-                
-                buttonValue = "";
-            }
-            
-            if (charArray2.includes(screenTextValuesLastChar) && tempStringLastChar == "−"){
-                buttonValue = tempStringLastChar;
-            }
-            //End
-
-            screenTextValues += buttonValue;
-            screenText.innerText = screenTextValues;
-            toggleBlinkingEffect();
-    }
-}
-
-function toggleBlinkingEffect(){
-    if (screenText.innerText == "|"){
-        screenText.classList.add("insertion-point");
-    }else{
-        screenText.classList.remove("insertion-point");
+            defaultBehaviour(buttonValue);
     }
 }
